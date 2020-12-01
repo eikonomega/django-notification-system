6aadcb9e291834a6ebcc42dab5c33e97e179b337
Justin Branco <jbranco@nd.edu>
2020-12-01 14:46:16 -0500

    Move check and retry to utils init

--- django_notification_system/utils/__init__.py
+++ django_notification_system/utils/__init__.py
@@ -1,4 +1,7 @@
+from dateutil.relativedelta import relativedelta
+
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 from django_notification_system.exceptions import UserIsOptedOut
 from django_notification_system.models.user_target import UserTarget
@@ -35,3 +38,33 @@ def user_notification_targets(user: User, target_name: str):
         target__name=target_name,
         active=True,
     )
+
+
+def check_and_update_retry_attempts(notification, minute_interval=None):
+    """
+    Check if the retry_attempt and max_retries are equal.
+    If they are we want to change the status to DELIVERY_FAILURE.
+    If not, we want to RETRY at a given minute interval.
+    """
+    if notification.max_retries != notification.retry_attempts:
+        notification.retry_attempts += 1
+        notification.save()
+        # We have to go deeper...
+        if notification.max_retries != notification.retry_attempts:
+            notification.status = notification.RETRY
+            if minute_interval is not None:
+                notification.scheduled_delivery = (
+                    timezone.now() + relativedelta(minutes=minute_interval)
+                )
+            else:
+                notification.scheduled_delivery = (
+                    timezone.now()
+                    + relativedelta(minutes=notification.retry_time_interval)
+                )
+            notification.attempted_delivery = timezone.now()
+            notification.save()
+
+        else:
+            notification.status = notification.DELIVERY_FAILURE
+            notification.attempted_delivery = timezone.now()
+            notification.save()
