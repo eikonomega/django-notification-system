4aabf2d9783a1f72069c622f8ba67db0ec2db9e3
Justin Branco <jbranco@nd.edu>
2020-12-01 16:21:32 -0500

    Tweaking to dynamic imports

--- django_notification_system/management/commands/process_notifications.py
+++ django_notification_system/management/commands/process_notifications.py
@@ -1,18 +1,15 @@
-import logging
+import importlib
+import inspect
 import os
+from os import path
 
 from django.conf import settings
-from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand
 from django.db.models import Q
 from django.utils import timezone
 
-from ...models import Notification, UserTarget
-from ...utils.notification_handlers import (
-    send_email, send_push_message)
-from website.database_apps.subject_manager.models import Subject
+from ...models import Notification
 
-logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     """
@@ -21,41 +18,64 @@ class Command(BaseCommand):
 
     help = __doc__
 
-    def handle(self, *args, **options):
-        # Get all SCHEDULED and RETRY notifications with a scheduled_delivery before the
-        # current date_time for development team if env is not production.
+    __function_table = {}
 
+    @classmethod
+    def _load_function_table(cls):
+        """
+
+        """
+        # TODO: This isn't currently working, and we'll need to add in where we want them to keep
+        # their handler functions
+        import django_notification_system.notification_handlers
+        handler_dir = path.dirname(inspect.getfile(
+            django_notification_system.notification_handlers))
+        for handle in os.listdir(path.join(handler_dir)):
+            if '__init__' in handle:
+                continue
+            temp = {}
+            try:
+                handler = importlib.import_module(
+                    f"django_notification_system.notification_handlers.{handle}")
+                print(getattr(handler, 'send_notification'))
+                temp['send_notification'] = getattr(handler, 'send_notification')
+            except (ModuleNotFoundError, AttributeError):
+                pass
+
+            cls.__function_table[handle.partition('.py')[0]] = temp
+
+    def handle(self, *args, **options):
+        print(self._load_function_table())
+        print(self.__function_table)
         # Get all SCHEDULED and RETRY notifications with a
         # scheduled_delivery before the current date_time
         notifications = Notification.objects.filter(
             Q(status='SCHEDULED') | Q(status='RETRY'),
             scheduled_delivery__lte=timezone.now())
 
-        if os.environ['ENVIRONMENT'] != 'production':
-            notifications = notifications.filter(user_target__user__is_superuser=True)
-
-        if getattr(settings, 'DISABLE_EMAILS', False):
-            notifications = notifications.exclude(user_target__target__name='Email')
-
         # excludes all notifications where the user has OptOut object with has_opted_out=True
-        notifications = notifications.exclude(user_target__user__notification_opt_out__has_opted_out=True)
+        notifications = notifications.exclude(
+            user_target__user__notification_opt_out__active=True)
+
         # Loop through each notification and attempt to push it
         for notification in notifications:
-            print(f"{notification.user_target.user.username} - {notification.scheduled_delivery} - {notification.status}", file=self.stdout)
-            print(f"{notification.title} - {notification.body}", file=self.stdout)
+            print(
+                f"{notification.user_target.user.username} - {notification.scheduled_delivery} - {notification.status}")
+            print(f"{notification.title} - {notification.body}")
+
             if not notification.user_target.active:
-                logger.debug('Tried to notify inactive target %s',  notification.user_target.target)
                 notification.status = Notification.INACTIVE_DEVICE
                 notification.save()
-            elif notification.user_target.target.name == 'Expo':
-                # Attempt to send push notifications to targets.
-                response_message = send_push_message(notification)
-                print(response_message, file=self.stdout)
-                print('*********************************', file=self.stdout)
-            elif notification.user_target.target.name == 'Email':
-                # Attempt to send email notifications to targets.
-                response_message = send_email(notification)
-                print(response_message, file=self.stdout)
-                print('*********************************', file=self.stdout)
             else:
-                logger.debug(f'invalid notification target name {notification.user_target.target.name}')
+                pass
+                # Dynamic imports....
+                function = notification.user_target.target.name.lower()
+                try:
+                    response_message = self.__function_table[function]['send_notification'](
+                        notification)
+                except KeyError:
+                    print(
+                        f'invalid notification target name {notification.user_target.target.name}')
+                else:
+                    print(response_message)
+                    print('*********************************')
