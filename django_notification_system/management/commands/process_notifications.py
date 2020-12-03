83fa84aeeca4604c29196d39f92e430a38b00cb0
Justin Branco <jbranco@nd.edu>
2020-12-03 16:24:56 -0500

    Clean up dynamic imports

--- django_notification_system/management/commands/process_notifications.py
+++ django_notification_system/management/commands/process_notifications.py
@@ -9,6 +9,9 @@ from django.db.models import Q
 from django.utils import timezone
 
 from ...models import Notification
+from ...notification_handlers.email import send_notification as send_email
+from ...notification_handlers.twilio import send_notification as send_twilio
+from ...notification_handlers.expo import send_notification as send_expo
 
 
 class Command(BaseCommand):
@@ -18,7 +21,11 @@ class Command(BaseCommand):
 
     help = __doc__
 
-    __function_table = {}
+    __function_table = {
+        "expo": send_expo,
+        "twilio": send_twilio,
+        "email": send_email
+    }
 
     @classmethod
     def _load_function_table(cls):
@@ -26,66 +33,34 @@ class Command(BaseCommand):
         This function will get our function table populated with all available `send_notification`
         functions.
         """
-        import django_notification_system.notification_handlers
-
-        # Get our handler directory. NOTE: We want to add one for the functions users will create
-        # to add to our table.
-        handler_dir = path.dirname(
-            inspect.getfile(django_notification_system.notification_handlers)
-        )
-
-        for file in os.listdir(path.join(handler_dir)):
-            if "__init__" in file:
-                # Ignore the init file
-                continue
-            try:
-                # Strip off the .py before importing the module
-                notification_system = file.partition(".py")[0]
-                module = importlib.import_module(
-                    f"django_notification_system.notification_handlers.{notification_system}"
-                )
-                print("Working Module", module.__dict__)
-                # Add the function to our function table
-                cls.__function_table[notification_system] = getattr(
-                    module, "send_notification"
-                )
-            except (ModuleNotFoundError, AttributeError):
-                pass
-
-        print("After First Loopie Loop", cls.__function_table)
-
         for directory in settings.NOTIFICATION_SYSTEM_HANDLERS:
-            for file in os.listdir(path.join(directory)):
-                print("Current File Being Evaluated", file)
-                if "__init__" in file:
-                    # Ignore the init file
-                    continue
-                try:
-                    module_spec = importlib.util.spec_from_file_location(
-                        file, f"{directory}/{file}"
-                    )
-                    print("Module Spec:", module_spec)
-                    module = importlib.util.module_from_spec(module_spec)
-                    print("Module:", module)
-                    module_spec.loader.exec_module(module)
-                    real_func = getattr(module, "send_notification")
-
-                    print("Function of Eggs, named Pepper", real_func)
-
-                    notification_system = file.partition(".py")[0]
-                    cls.__function_table[notification_system] = real_func
-
-                    # Add the function to our function table
-                    # cls.__function_table[notification_system] = real_func
-                except (ModuleNotFoundError, AttributeError) as error:
-                    print("FAILURE", error)
-                    # print(getattr(module, "send_notification"))
-                    pass
+            try:
+                for file in os.listdir(path.join(directory)):
+                    if "__init__" in file:
+                        # Ignore the init file
+                        continue
+                    try:
+                        # Create the module spec
+                        module_spec = importlib.util.spec_from_file_location(
+                            file, f"{directory}/{file}")
+                        # Create a new module  based  on the spec
+                        module = importlib.util.module_from_spec(module_spec)
+                        # An abstract method that executes the module
+                        module_spec.loader.exec_module(module)
+                        #  Get the actual function
+                        real_func = getattr(module, "send_notification")
+                        # Add it to our dictionary of functions
+                        notification_system = file.partition(".py")[0]
+                        cls.__function_table[notification_system] = real_func
+                    except (ModuleNotFoundError, AttributeError):
+                        pass
+            except FileNotFoundError:
+                # the directory provided in the settings file does not exist
+                pass
 
     def handle(self, *args, **options):
         # Load the function table
         self._load_function_table()
-        print(self.__function_table)
 
         # Get all SCHEDULED and RETRY notifications with a
         # scheduled_delivery before the current date_time
@@ -95,15 +70,12 @@ class Command(BaseCommand):
         )
 
         # excludes all notifications where the user has OptOut object with has_opted_out=True
-        notifications = notifications.exclude(
-            user_target__user__notification_opt_out__active=True
-        )
+        notifications = notifications.exclude(user_target__user__notification_opt_out__active=True)
 
         # Loop through each notification and attempt to push it
         for notification in notifications:
             print(
-                f"{notification.user_target.user.username} - {notification.scheduled_delivery} - {notification.status}"
-            )
+                f"{notification.user_target.user.username} - {notification.scheduled_delivery} - {notification.status}")
             print(f"{notification.title} - {notification.body}")
 
             if not notification.user_target.active:
@@ -115,13 +87,10 @@ class Command(BaseCommand):
                 )
                 try:
                     # Use our function table to call the appropriate sending function
-                    response_message = self.__function_table[notification_type](
-                        notification
-                    )
+                    response_message = self.__function_table[notification_type](notification)
                 except KeyError:
                     print(
-                        f"invalid notification target name {notification.user_target.target.name}"
-                    )
+                        f"invalid notification target name {notification.user_target.target.name}")
                 else:
                     # The notification was sent successfully
                     print(response_message)
