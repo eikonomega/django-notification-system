05fd12288e2af521a6fa68c5872bad751ab66705
Justin Branco <jbranco@nd.edu>
2020-12-15 11:36:21 -0500

    Almost ready

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     # This is the name of your project. The first time you publish this
@@ -20,7 +20,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1',  # Required
+    version='1.0.0',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -42,7 +42,7 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=["django_notification_system"],  # Required
+    packages=find_packages(include=['django_notification_system', 'django_notification_system.*']),  # Required
 
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
